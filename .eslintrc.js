2c8477551e95997ca755d08f2dea0e1715012369
Diego Luiz <diegoluizcsti@gmail.com>
2019-12-10 17:35:10 -0300

    configurando o rectotron com o redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     "react/jsx-filename-extension": [
       "warn", { extensions: [".js", ".jsx"] }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-console': ["error", {allow: ["tron"]}]
   },
 };
