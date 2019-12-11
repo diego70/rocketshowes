4662172873eea71c554da4cbcf8dc02d263c75f2
Diego Luiz <diegoluizcsti@gmail.com>
2019-12-11 15:03:01 -0300

    refatorando actions

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import {
   MdRemoveCircleOutline,
@@ -6,12 +7,11 @@ import {
   MdDelete,
 } from 'react-icons/md';
 
+import * as CartActions from '../../store/modules/cart/actions';
+
 import { Container, ProductTable, Total } from './styles';
 
-const image =
-  'https://static.netshoes.com.br/produtos/tenis-puma-mapm-kart/44/D14-9603-244/D14-9603-244_detalhe2.jpg?ims=326x';
-
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -51,9 +51,7 @@ function Cart({ cart, dispatch }) {
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
-                  }
+                  onClick={() => removeFromCart(product.id)}
                 >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
@@ -77,5 +75,7 @@ function Cart({ cart, dispatch }) {
 const mapStateToProps = state => ({
   cart: state.cart,
 });
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
 
-export default connect(mapStateToProps)(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
