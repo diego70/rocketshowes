40a95637ac9cdb52b3223794877f7b231ee8cfc4
Diego Luiz <diegoluizcsti@gmail.com>
2019-12-12 14:48:31 -0300

    valor de quantidade no item no butão de carrinho em cada li

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -35,6 +35,8 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
+
     return (
       <ProductList>
         {products.map(product => (
@@ -48,7 +50,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdShoppingCart size={16} color="#fff" /> 3
+                <MdShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CAMINHO</span>
             </button>
@@ -59,7 +62,14 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
